ef33db709384d20141bcceca2618ae312260ba4f
egsch <egschaumann@gmail.com>
2024-10-18 21:27:07 -0500

    remove wrong header prop

--- GuideMe/app/_layout.tsx
+++ GuideMe/app/_layout.tsx
@@ -2,7 +2,7 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{navigationBarHidden: true}}>
+    <Stack>
       <Stack.Screen name="index" options={{headerShown: false}}/>
       <Stack.Screen name="login" options={{title: "Log In"}}/>
       <Stack.Screen name="signup" options={{title: "Sign Up"}}/>
