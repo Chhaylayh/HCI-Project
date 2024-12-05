fca6aea7b665b6a19ad5d31abe0fd2cfb9d2c886
kayl102 <148817078+kayl102@users.noreply.github.com>
2024-12-04 19:33:28 -0600

    Update _layout.tsx
    layout

--- GuideMe/app/(auth)/home/_layout.tsx
+++ GuideMe/app/(auth)/home/_layout.tsx
@@ -1,13 +1,59 @@
-import { router, Tabs } from "expo-router";
+import { StatusBar } from "react-native";
+import { Tabs } from "expo-router";
 import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function TabLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="dashboard" options={{ headerShown: false, title: "Home", tabBarIcon: ({color})=><Ionicons name="home-outline" size={24} color={color}/>}} />
-      <Tabs.Screen name="project" options={{ headerShown: false, title: "My Projects", tabBarIcon: ({color})=><Ionicons name="checkbox-outline" size={24} color={color}/>}} />
-      <Tabs.Screen name="search" options={{ headerShown: false, title: "Search", tabBarIcon: ({color})=><Ionicons name="search-outline" size={24} color={color}/>}} />
-      <Tabs.Screen name="profile" options={{ headerShown: false, title: "Profile", tabBarIcon: ({color})=><Ionicons name="person-outline" size={24} color={color}/>}} />
-    </Tabs>
+    <>
+      <StatusBar backgroundColor="darkblue" barStyle="light-content" />
+      <Tabs
+        screenOptions={{
+          tabBarStyle: { backgroundColor: 'darkblue' }, 
+          tabBarActiveTintColor: '#fff', 
+          tabBarInactiveTintColor: '#ECEDEE', 
+        }}
+      >
+        <Tabs.Screen
+          name="dashboard"
+          options={{
+            headerShown: false,
+            title: "Home",
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="home-outline" size={24} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="project"
+          options={{
+            headerShown: false,
+            title: "My Projects",
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="checkbox-outline" size={24} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="search"
+          options={{
+            headerShown: false,
+            title: "Search",
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="search-outline" size={24} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            headerShown: false,
+            title: "Profile",
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="person-outline" size={24} color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+    </>
   );
 }
